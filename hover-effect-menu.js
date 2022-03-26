df5787e956bea8fe52217a49ad6449ea718d15ff
Tmillion <97577481+Tmillion@users.noreply.github.com>
2022-03-26 15:08:02 +0600

    Delete hover-effect-menu.js

--- hover-effect-menu.js
+++
@@ -1,6 +0,0 @@
-
-const selectElement = (element) => document.querySelector(element);
-
-selectElement('.main-icon').addEventListener('click', () => { console.log('click')
-  selectElement('nav').classList.toggle('active');
-});
